ea0b7cc473bc7aafd7dcaca780169e00eb7237ad
Sebas Lima <cabreralimasebastian123@gmail.com>
2026-04-24 02:00:41 -0500

    Teoria de la complejidad

---
+++ src/O5Ejemplo.java
@@ -0,0 +1,12 @@
+public class O5Ejemplo {
+    public void ejemplo() {
+        int n = 16;
+        while (n > 1) {
+            n = n / 2;
+            System.out.println(n);
+        }
+    }
+}
+
+
+
